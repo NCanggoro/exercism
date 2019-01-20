ca6bef44c3cac1d42f96724b6775c91f399a4f83
NCanggoro <figuranajah@gmail.com>
2019-01-20 22:16:11 +0700

    add csharp exercism

---
+++ csharp/hamming/Hamming.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        //implement hamming
+ 
+    }
+}
---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var newString = "";
+        for(int i = input.Length - 1; i >= 0; i--)
+        {
+            newString += input[i];
+        }
+
+        return newString;
+    }
+}
