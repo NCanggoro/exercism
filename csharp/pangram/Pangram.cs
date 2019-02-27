265acfb656f16e2d22bc80c26fef4d01017fa248
NCanggoro <figuranajah@gmail.com>
2019-02-27 16:53:46 +0700

    add pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        const int Alphabetsize = 26;
+
+        return input
+            .ToLower()
+            .Distinct()
+            .Count(char.IsLetter) == Alphabetsize;
+    }
+}
