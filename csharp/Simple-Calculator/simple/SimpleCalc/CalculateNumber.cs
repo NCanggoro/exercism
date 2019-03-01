25d36629fa6ae824824aac23aaa3f6073d7a8c2d
NCanggoro <figuranajah@gmail.com>
2019-03-01 15:25:54 +0700

    add simple calculator console app

---
+++ csharp/Simple-Calculator/simple/SimpleCalc/CalculateNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    public class CalculateNumber
+    {
+        public double Calculate(string method, double firstNumber, double SecondNumber)
+        {
+            double result;
+
+            switch (method.ToLower())
+            {
+                case "tambah":
+                case "+":
+                    result = firstNumber + SecondNumber;
+                    break;
+                case "kurang":
+                case "pengurangan":
+                case "-":
+                    result = firstNumber - SecondNumber;
+                    break;
+                case "pembagian":
+                case "bagi":
+                case "/":
+                    result = firstNumber / SecondNumber;
+                    break;
+                case "perkalian":
+                case "*":
+                case "x":
+                    result = firstNumber * SecondNumber;
+                    break;
+                default:
+                    throw new Exception("Wrong Method {*,/,+,-}");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ csharp/Simple-Calculator/simple/SimpleCalc/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ConvertInput convertInput = new ConvertInput();
+                CalculateNumber calculateNumber = new CalculateNumber();
+
+                double firstNumber = convertInput.Convert(Console.ReadLine());
+                double secondNumber = convertInput.Convert(Console.ReadLine());
+                string method = Console.ReadLine();
+
+                double result = calculateNumber.Calculate(method, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //log the message
+                Console.WriteLine(ex.Message);
+
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
