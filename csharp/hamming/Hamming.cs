4aa61360828bd2ec580a22397527384eeae2536f
NCanggoro <figuranajah@gmail.com>
2019-03-04 21:10:21 +0700

    edit hamming

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -6,10 +6,10 @@ public static class Hamming
     public static int Distance(string firstStrand, string secondStrand)
     {
         if (firstStrand.Length > secondStrand.Length)
-            throw new ArgumentException($"first strand {firstStrand.Length} is too many");
+            throw new ArgumentException($"first strand {firstStrand.Length} is longer second strand");
 
         if (secondStrand.Length > firstStrand.Length)
-            throw new ArgumentException($"Second Strand {secondStrand.Length} is too many");
+            throw new ArgumentException($"Second Strand {secondStrand.Length} is longer than first strand");
 
         return firstStrand
                .Zip(secondStrand, Tuple.Create)
